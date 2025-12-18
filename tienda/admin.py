09bab0cbd7475bb07f224024ca9fc922cf54dda9
Omar Sepúlveda <129816268+OmarSepulveda1@users.noreply.github.com>
2025-12-18 02:58:20 -0300

    Delete tienda directory

--- tienda/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Producto, Categoria, Etiqueta, DetalleProducto
-
-admin.site.register(Producto)
-admin.site.register(Categoria)
-admin.site.register(Etiqueta)
-admin.site.register(DetalleProducto)
